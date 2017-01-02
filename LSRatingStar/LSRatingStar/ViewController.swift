32089e0a13c2164a5da6618e62b56b1aa065870c
John_LS <tianyc1019@icloud.com>
2017-01-02 11:06:30 +0800

    创建项目

---
+++ LSRatingStar/LSRatingStar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LSRatingStar
+//
+//  Created by John_LS on 2017/1/2.
+//  Copyright © 2017年 John_LS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
